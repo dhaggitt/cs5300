29b29ed9a0dae3f7843b9a9cc3dd5aa0ad285036
dhaggitt <uofa0603@yahoo.com>
2026-02-17 19:35:56 +0000

    Forgot tests for other two functions

--- homework1/tests/test_task3.py
+++ homework1/tests/test_task3.py
@@ -9,3 +9,11 @@ def test_output(capsys):
     assert task3.check_sign(0) is "zero"
     assert task3.check_sign(5) is "positive"
     assert task3.check_sign(999999) is "positive"
+
+    # Tests the first ten primes function
+    task3.first_ten_prime()
+    captured = capsys.readouterr()
+    assert captured.out == "1\n2\n3\n5\n7\n11\n13\n17\n19\n23\n"
+
+    # Tests the century_sum function
+    assert task3.century_sum() == 5050
