2052d5c8b620939cb0f44b82bfc6abe1555547f4
Devin Haggitt <dhaggitt@uccs.edu>
2026-03-06 06:03:30 +0000

    Importing Bootstrap Template

--- homework2/django-hw2/bookings/views.py
+++ homework2/django-hw2/bookings/views.py
@@ -2,7 +2,17 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Movie
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the bookings index.")
+    latest_movie_list = Movie.objects.order_by("-release_date")[:5]
+    template = loader.get_template("bookings/index.html")
+    context = {"latest_movie_list": latest_movie_list}
+    return HttpResponse(template.render(context, request))
+
+
+def detail(request, movie_id):
+    return HttpResponse("You're looking at movie %s.", movie_id)
